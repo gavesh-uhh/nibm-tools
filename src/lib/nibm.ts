dd3898c7286ddae5925a61887c675716584a81f5
Gavesh Saparamadu <gaveshasaparamadu06@gmail.com>
2025-02-24 21:23:17 +0530

    Changed. moved font to local folder for offline use

--- src/lib/nibm.ts
+++ src/lib/nibm.ts
@@ -7,10 +7,9 @@ const getDay = (currentDate: Date | undefined, offset: number): string => {
 
 const getRandomQuote = () => {
   const quotes = [
-    'Why hurry? Your lecturer will start 10 minutes late anyway.',
+    'Why hurry? Your lecturer will start 15 minutes late anyway.',
     'Fun fact: This page is as reliable as the Wi-Fi in the lecture halls.',
     'If this takes too long, blame the admin department.',
-    'Loading... because skipping class isn’t an option. Or is it?',
     'Still loading... just like the uni elevators.',
     'If you can read this, you’re probably not in a lecture.',
     'Not saying names, but someone should stop watching kids through CCTVs',
